7b0365decad5a6634a7b17af68ca01ac33485b4e
Paúl Valverde <paul-vlvrd@hotmail.com>
2026-03-10 23:28:54 -0400

    Se agregaron husky y validaciones pre-commit

--- src/main/java/edu/nur/nurtricenter_appointment/application/appointments/attendAppointment/AttendAppointmentHandler.java
+++ src/main/java/edu/nur/nurtricenter_appointment/application/appointments/attendAppointment/AttendAppointmentHandler.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 
 import an.awesome.pipelinr.Command;
 import edu.nur.nurtricenter_appointment.application.utils.DiagnosisMapper;
-import edu.nur.nurtricenter_appointment.application.utils.MealPlanMapper;
 import edu.nur.nurtricenter_appointment.application.utils.MeasurementMapper;
 import edu.nur.nurtricenter_appointment.core.results.ResultWithValue;
 import edu.nur.nurtricenter_appointment.domain.appointments.Appointment;
@@ -16,33 +15,35 @@ import edu.nur.nurtricenter_appointment.core.results.Error;
 
 @Component
 public class AttendAppointmentHandler implements Command.Handler<AttendAppointmentCommand, ResultWithValue<Boolean>> {
-  private final IAppointmentRepository appointmentRepository;
-  private final UnitOfWorkJpa unitOfWork;
-  
+	private final IAppointmentRepository appointmentRepository;
+	private final UnitOfWorkJpa unitOfWork;
 
-  public AttendAppointmentHandler(IAppointmentRepository appointmentRepository, UnitOfWorkJpa unitOfWork) {
-    this.appointmentRepository = appointmentRepository;
-    this.unitOfWork = unitOfWork;
-  }
+	public AttendAppointmentHandler(IAppointmentRepository appointmentRepository, UnitOfWorkJpa unitOfWork) {
+		 this.appointmentRepository = appointmentRepository;
+		this.unitOfWork = unitOfWork;
+	}
 
-  @Override
-  public ResultWithValue<Boolean> handle(AttendAppointmentCommand request) {
-    Appointment appointment = this.appointmentRepository.GetById(request.id());
-    if (appointment == null) return ResultWithValue.failure(Error.notFound("Appointment.NotFound", "The appointment was not found", request.id().toString()));
-    try {
-    appointment.attend(
-      request.notes(), 
-      MeasurementMapper.from(request.measurementDto()), 
-      DiagnosisMapper.from(request.diagnosisDto())
-      // MealPlanMapper.from(request.mealPlanDto())
-    );
-    } catch(DomainException e) {
-      Error err = e.getError();
-      return ResultWithValue.failure(Error.failure(err.getCode(), err.getStructuredMessage(), request.id().toString()));
-    }
-    this.appointmentRepository.update(appointment);
-    this.unitOfWork.commitAsync();
-    appointment.addDomainEvent(new AppointmentAttendedEvent(appointment.getId(), appointment.getMeasurement(), appointment.getDiagnosis()));
-    return ResultWithValue.success(true);
-  }
+	@Override
+	public ResultWithValue<Boolean> handle(AttendAppointmentCommand request) {
+		Appointment appointment = this.appointmentRepository.GetById(request.id());
+		if (appointment == null)
+			return ResultWithValue
+					.failure(Error.notFound("Appointment.NotFound", "The appointment was not found", request.id().toString()));
+		try {
+			appointment.attend(
+					request.notes(),
+					MeasurementMapper.from(request.measurementDto()),
+					DiagnosisMapper.from(request.diagnosisDto())
+			// MealPlanMapper.from(request.mealPlanDto())
+			);
+		} catch (DomainException e) {
+			Error err = e.getError();
+			return ResultWithValue.failure(Error.failure(err.getCode(), err.getStructuredMessage(), request.id().toString()));
+		}
+		this.appointmentRepository.update(appointment);
+		this.unitOfWork.commitAsync();
+		appointment.addDomainEvent(
+				new AppointmentAttendedEvent(appointment.getId(), appointment.getMeasurement(), appointment.getDiagnosis()));
+		return ResultWithValue.success(true);
+	}
 }
