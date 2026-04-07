7b665c16bea47507ce8b35881bc447158bf94752
Paúl Valverde <paul-vlvrd@hotmail.com>
2026-04-06 23:20:28 -0400

    [ADD] Grafana

--- src/main/java/edu/nur/nurtricenter_appointment/webapi/config/SecurityConfig.java
+++ src/main/java/edu/nur/nurtricenter_appointment/webapi/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
 		http
 			.csrf(csrf -> csrf.disable())
 			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/actuator/**").permitAll()
 				.anyRequest().authenticated()
 			)
 			.oauth2ResourceServer(oauth2 ->
